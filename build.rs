dd3dcbc613b17a4b17af709c112dcd9b996bc0b4
AlTeveDev <73194036+AlTeveDev@users.noreply.github.com>
2024-07-10 03:20:30 +0300

    Add files via upload

---
+++ build.rs
@@ -0,0 +1,39 @@
+use std::fs::{
+	copy,
+	read_dir,
+	create_dir,
+	remove_file,
+};
+use std::path::PathBuf;
+
+
+fn recursive_copy(from: PathBuf, to: PathBuf) -> Result<(), std::io::Error> {
+	let items = read_dir(from)?;
+	for item in items {
+		let path: PathBuf = item?.path();
+		
+		let name = path.file_name().expect("File has name");
+		let new_to = to.join(name);
+			
+		if path.is_dir() {
+			if !new_to.exists() {
+				create_dir(new_to.clone())?;
+			}
+			recursive_copy(path, new_to)?;
+		} else {
+			println!("{:?}", &new_to);
+			if new_to.exists() {
+				remove_file(&new_to)?;
+			}
+			copy(path, new_to)?;
+		}
+	}
+	
+	Ok(())
+}
+
+fn main() {
+	recursive_copy("html".into(), "pkg".into()).unwrap();
+	println!("cargo::rerun-if-changed=html/");
+	println!("cargo::rerun-if-changed=build.rs");
+}
